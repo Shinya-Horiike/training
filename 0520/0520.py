40db3d38d2ea028ca549b77b2c96831df2f3ec44
unknown <1217.shinya.horiike.5@gmail.com>
2020-05-21 13:24:04 +0900

    WORLD_trainging

--- 0520/0520.py
+++ 0520/0520.py
@@ -12,7 +12,12 @@ f0 = pw.stonemask(data, _f0, t, fs)
 sp = pw.cheaptrick(data, f0, t, fs)
 ap = pw.d4c(data, f0, t, fs)
 
-synthesized = pw.synthesize(f0, sp, ap, fs)
+conversion_f0 = f0*1.0  #数値を変えることでF0が変化
+conversion_sp = np.zeros_like(sp)  
+for f in range(conversion_sp.shape[1]):
+    conversion_sp[:, f] = sp[:, int(f/1.0)]  #int内の分母の数値を変えることでSPが変化
+
+synthesized = pw.synthesize(conversion_f0, conversion_sp, ap, fs)
 synthesized = synthesized.astype(np.int16) #データ型を戻す必要あり
 
 wavfile.write(WAV_FILE+"_conversion.wav", fs, synthesized)
